64e08eb28e4f49f2d4c7fccab44e562409cdf250
Célio Júnior <celio_cjr@hotmail.com>
2024-05-31 19:18:11 +0930

    feat: creating transition method and making the slide stop smoothly in the current target.

--- js/slide.js
+++ js/slide.js
@@ -9,6 +9,10 @@ export default class Slide {
         }
     }
 
+    transition(active) {
+        this.slide.style.transition = active ? 'transform .3s' : '';
+    }
+
     moveSlide(distX) {
         this.dist.movePosition = distX;
         this.slide.style.transform = `translate3d(${distX}px, 0px, 0px)`;
@@ -30,6 +34,7 @@ export default class Slide {
             moveType = 'touchend';
         }
         this.wrapper.addEventListener(moveType, this.onMove);
+        this.transition(false);
     }
 
     onMove(event) {
@@ -42,12 +47,18 @@ export default class Slide {
         const moveType = (event.type === 'mouseup') ? 'mousemove' : 'touchmove';
         this.wrapper.removeEventListener(moveType, this.onMove);
         this.dist.finalPosition = this.dist.movePosition;
+        this.transition(true);
         this.changeSlideOnEnd();
     }
 
     changeSlideOnEnd() {
-        if(this.dist.movement);
-        console.log(this.dist.movement)
+        if(this.dist.movement > 120 && this.index.next !== undefined) {
+            this.activeNextSlide();
+        } else if(this.dist.movement < 120 && this.index.prev !== undefined) {
+            this.activePrevSlide();
+        } else {
+            this.changeSlide(this.index.active);
+        }
     }
 
     addSlideEvents() {
@@ -66,7 +77,7 @@ export default class Slide {
     // Slides Config
     slidePosition(slide) {
         const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2;
-        return  -(slide.offsetLeft - margin);
+        return  - (slide.offsetLeft - margin);
     }
 
     slidesConfig() {
@@ -106,6 +117,7 @@ export default class Slide {
 
     init() {
         this.bindEvents();
+        this.transition(true);
         this.addSlideEvents();
         this.slidesConfig();
 
